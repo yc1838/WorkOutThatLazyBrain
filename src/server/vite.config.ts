f1d1a65f2ce8cc96a4d41761db4f87cc418d1be3
Marcus Wood <marcus.wood@reddit.com>
2025-06-12 13:11:08 -0400

    prettier

--- src/server/vite.config.ts
+++ src/server/vite.config.ts
@@ -1,14 +1,14 @@
-import { defineConfig } from "vite";
-import { builtinModules } from "node:module";
+import { defineConfig } from 'vite';
+import { builtinModules } from 'node:module';
 
 export default defineConfig({
   ssr: {
     noExternal: true,
   },
   build: {
-    ssr: "index.ts",
-    outDir: "../../dist/server",
-    target: "node22",
+    ssr: 'index.ts',
+    outDir: '../../dist/server',
+    target: 'node22',
     sourcemap: true,
     // Do not turn on as it will break asset loading inside of devvit since
     // we watch an individual file instead of the entire directory.
@@ -18,8 +18,8 @@ export default defineConfig({
       external: [...builtinModules],
 
       output: {
-        format: "cjs",
-        entryFileNames: "index.cjs",
+        format: 'cjs',
+        entryFileNames: 'index.cjs',
         inlineDynamicImports: true,
       },
     },
