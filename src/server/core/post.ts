ecabe8a27474a0f262093a4c86e9380218967430
Marcus Wood <marcus.wood@reddit.com>
2025-07-29 14:08:14 -0400

    splash screen

--- src/server/core/post.ts
+++ src/server/core/post.ts
@@ -1,4 +1,3 @@
-import { Devvit } from '@devvit/public-api';
 import { context } from '@devvit/server';
 import { reddit } from '@devvit/reddit';
 
@@ -9,20 +8,10 @@ export const createPost = async () => {
   }
 
   return await reddit.submitPost({
-    userGeneratedContent: {
-      text: 'Hello there! This is a post from a Devvit app',
+    splash: {
+      appDisplayName: '<% name %>',
     },
     subredditName: subredditName,
-    title: 'New Post',
-    preview: Devvit.createElement('blocks', {
-      height: 'tall',
-      children: [
-        Devvit.createElement('vstack', {
-          height: '100%',
-          width: '100%',
-          backgroundColor: '#ffbf0b',
-        }),
-      ],
-    }),
+    title: '<% name %>',
   });
 };
