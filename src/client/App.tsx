446b2026a23bad4afe859ab119a290d01b959646
Marcus Wood <marcus.wood@reddit.com>
2025-07-31 15:03:46 -0400

    rm dark mode

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -7,15 +7,12 @@ export const App = () => {
     <div className="flex relative flex-col justify-center items-center min-h-screen gap-4">
       <img className="object-contain w-1/2 max-w-[250px] mx-auto" src="/snoo.png" alt="Snoo" />
       <div className="flex flex-col items-center gap-2">
-        <h1 className="text-2xl font-bold text-center text-gray-900 dark:text-gray-100">
+        <h1 className="text-2xl font-bold text-center text-gray-900 ">
           {username ? `Hey ${username} 👋` : ''}
         </h1>
-        <p className="text-base text-center text-gray-600 dark:text-gray-300">
-          Edit{' '}
-          <span className="bg-[#e5ebee] dark:bg-gray-700 dark:text-gray-100 px-1 py-0.5 rounded">
-            src/client/App.tsx
-          </span>{' '}
-          to get started.
+        <p className="text-base text-center text-gray-600 ">
+          Edit <span className="bg-[#e5ebee]  px-1 py-0.5 rounded">src/client/App.tsx</span> to get
+          started.
         </p>
       </div>
       <div className="flex items-center justify-center mt-5">
@@ -26,7 +23,7 @@ export const App = () => {
         >
           -
         </button>
-        <span className="text-[1.8em] font-medium mx-5 min-w-[50px] text-center leading-none text-gray-900 dark:text-gray-100">
+        <span className="text-[1.8em] font-medium mx-5 min-w-[50px] text-center leading-none text-gray-900">
           {loading ? '...' : count}
         </span>
         <button
@@ -37,21 +34,21 @@ export const App = () => {
           +
         </button>
       </div>
-      <footer className="absolute bottom-4 left-1/2 -translate-x-1/2 flex gap-3 text-[0.8em] text-gray-600 dark:text-gray-400">
+      <footer className="absolute bottom-4 left-1/2 -translate-x-1/2 flex gap-3 text-[0.8em] text-gray-600">
         <button
           className="cursor-pointer"
           onClick={() => navigateTo('https://developers.reddit.com/docs')}
         >
           Docs
         </button>
-        <span className="text-gray-300 dark:text-gray-500">|</span>
+        <span className="text-gray-300">|</span>
         <button
           className="cursor-pointer"
           onClick={() => navigateTo('https://www.reddit.com/r/Devvit')}
         >
           r/Devvit
         </button>
-        <span className="text-gray-300 dark:text-gray-500">|</span>
+        <span className="text-gray-300">|</span>
         <button
           className="cursor-pointer"
           onClick={() => navigateTo('https://discord.com/invite/R7yu2wh9Qz')}
